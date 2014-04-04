746e03e40ef94ded56184feeb0c176428354c25a
Matt Rice <mrice1@midmich.edu>
2014-04-04 15:44:54 -0400

    Fixed get_context message

--- block_quickfindlist.php
+++ block_quickfindlist.php
@@ -97,7 +97,7 @@ class block_quickfindlist extends block_base {
             $this->title = $strallusers.$strlist;
         }
 
-        $context_system = get_context_instance(CONTEXT_SYSTEM);
+        $context_system = context_system::instance();
 
         if (has_capability('block/quickfindlist:use', $context_system)) {
             if (empty($this->config->userfields)) {
