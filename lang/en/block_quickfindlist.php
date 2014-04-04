deb7b0b0adf1046da2e6c70fc8c9ff9ace8e1e6b
Matt Rice <mrice1@midmich.edu>
2014-04-04 16:26:35 -0400

    Added missing permission string

--- lang/en/block_quickfindlist.php
+++ lang/en/block_quickfindlist.php
@@ -33,6 +33,7 @@ $string['lotsofusers']         = ' WARNING: lots of users ({$a}) could get slow'
 $string['multiplenorole']         = 'You cannot have two blocks on the same page without a role configured; Edit this block and select a role.';
 $string['nousers']         =  'ERROR: No users have that role';
 $string['quickfindlist']         = 'Quickfind List';
+$string['quickfindlist:addinstance'] = 'Add a new Quickfind list block';
 $string['quickfindlist:use']         = 'Use Quickfind List';
 $string['role']         = 'Role for listed people to have';
 $string['url']         = 'Page to link to (the person\'s id will be appended to the end).<br />Leave blank for default profile';
